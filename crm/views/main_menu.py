24331ce40ecd6c1350e2872902f1c7777ffd4974
Steve <steve.raharison@gmail.com>
2025-01-24 10:39:38 +0300

    Main Menu Setup

--- crm/views/main_menu.py
+++ crm/views/main_menu.py
@@ -1,6 +1,28 @@
-from rich.console import Console
-from rich.table import Table
 from rich.prompt import Prompt
+from rich.console import Console
+
+from crm.controllers import (
+    client_controller,
+    contract_controller,
+    event_controller,
+    user_controller,
+    auth_controller
+)
+from crm.views import (
+    client_views,
+    contract_views,
+    event_views,
+    user_views
+)
+from db.session import get_db
 
 console = Console()
 
+
+def main_menu(user):
+    """
+    Display the main menu
+    """
+    db = next(get_db())
+
+    while True:
